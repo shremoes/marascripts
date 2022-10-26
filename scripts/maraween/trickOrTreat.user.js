b60e5f07c1ea4b221385b1064c1423c92229cfa4
Billy <will.js@outlook.com>
2022-10-26 15:52:40 -0500

    Trick or treat!

---
+++ scripts/maraween/trickOrTreat.user.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name        Trick or Treater
+// @namespace   Marascripts
+// @description Trick or treats.
+// @author      marascripts
+// @version     1.0.0
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_registerMenuCommand
+// @match       https://www.marapets.com/profile.php?id=*
+// @match       https://www.marapets.com/online.php
+// @run-at      document-idle
+// @downloadURL https://raw.githubusercontent.com/marascript/userscripts/master/scripts/maraween/trickOrTreat.user.js
+// @homepageURL https://github.com/marascript/userscripts
+// @supportURL	https://github.com/marascript/userscripts/issues
+// @license     MIT
+// ==/UserScript==
+/*jshint -W033 */
+/*jshint -W117 */ // Avoid JSHint errors in editor for GM* functions
+
+(function () {
+    'use strict'
+
+    const visited = GM_getValue("visited", [])
+
+    GM_registerMenuCommand("Reset visited", function () { GM_setValue("visited", []) })
+
+    if (document.URL.includes("online")) {
+        const allOnline = document.querySelectorAll(".mainfeature_start a")
+
+        for (const profile in allOnline) {
+            const profileLink = allOnline[profile].href
+            if (visited.includes(profileLink)) {
+                allOnline[profile].href = ""
+            }
+        }
+
+        for (const profile in allOnline) {
+            const href = allOnline[profile].href
+            if (href && href !== "" && href.includes("id=")) {
+
+                location.href = allOnline[profile].href + "&halloween=1"
+                break
+            }
+        }
+    }
+
+    if (document.URL.includes("profile")) {
+        visited.push(location.href.split("&")[0])
+        GM_setValue("visited", visited)
+        setTimeout(() => {
+            location.href = "https://www.marapets.com/online.php"
+        }, 1500)
+    }
+})()
