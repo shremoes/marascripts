b8df25d56b900bfb19e4a3fd2b80eabb697a4a18
Billy <will.js@outlook.com>
2022-10-27 13:33:06 -0500

    Add counter for pumpkins

--- scripts/maraween/pumpkinClicker.user.js
+++ scripts/maraween/pumpkinClicker.user.js
@@ -3,8 +3,9 @@
 // @namespace   Marascripts
 // @description Clicks pumpkins as they appear around the site.
 // @author      marascripts
-// @version     1.0.5
-// @grant       none
+// @version     1.1.0
+// @grant       GM_setValue
+// @grant       GM_getValue
 // @match       https://www.marapets.com/*
 // @run-at      document-idle
 // @downloadURL https://raw.githubusercontent.com/marascript/userscripts/master/scripts/maraween/pumpkinClicker.user.js
@@ -13,6 +14,7 @@
 // @license     MIT
 // ==/UserScript==
 /*jshint -W033 */
+/*jshint -W117 */ // Avoid JSHint errors in editor for GM* functions
 
 (function () {
     'use strict'
@@ -22,8 +24,11 @@
      */
     const IMG_SRC = "https://images.marapets.com/items/2022-pumpkin.png"
 
+    let clicked = GM_getValue("clickedPumpkins", 0)
     const pumpkin = document.querySelector(`.pumpkineventbox img[src='${IMG_SRC}']`)
     if (pumpkin) {
+        clicked += 1
+        GM_setValue("clickedPumpkins", clicked)
         window.open(pumpkin.parentElement.href, 'pumpkin', 'width=100,height=100,top=0,left=0,resizable=no')
     }
 })()
