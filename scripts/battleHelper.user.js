d2d7f5bb68cdc73aa7c8e12d18fb9dece723393a
Marascript <will.js@outlook.com>
2023-01-06 13:23:59 -0600

    Yup

--- scripts/battleHelper.user.js
+++ scripts/battleHelper.user.js
@@ -3,7 +3,7 @@
 // @namespace   Marascripts
 // @description Automates battling.
 // @author      marascripts
-// @version     1.1.0
+// @version     1.1.1
 // @require     https://raw.githubusercontent.com/marascript/userscripts/master/scripts/utilities/captcha.js
 // @grant       none
 // @match       https://www.marapets.com/battle.php*
