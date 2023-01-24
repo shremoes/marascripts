df6774cff88e3d5e0139ce372f60c880ef72b648
Billy <will.js@outlook.com>
2023-01-24 10:31:18 -0600

    Add all game pages

--- scripts/hideUnavailable.user.js
+++ scripts/hideUnavailable.user.js
@@ -3,11 +3,19 @@
 // @namespace   Marascripts
 // @description Always hides unavailable games/dailies.
 // @author      marascripts
-// @version     2.1.0
+// @version     1.0.1
 // @grant       none
 // @match       https://www.marapets.com/free_games.php
 // @match       https://www.marapets.com/pay_games.php
+// @match       https://www.marapets.com/arcade.php
+// @match       https://www.marapets.com/level_games.php
 // @match       https://www.marapets.com/pet_dailies.php
+// @match       https://www.marapets.com/chance_games.php
+// @match       https://www.marapets.com/classic_games.php
+// @match       https://www.marapets.com/premium.php
+// @match       https://www.marapets.com/feature_games.php
+// @match       https://www.marapets.com/all_games.php
+// @match       https://www.marapets.com/favourites.php
 // @downloadURL https://raw.githubusercontent.com/marascript/userscripts/master/scripts/hideUnavailable.user.js
 // @homepageURL https://github.com/marascript/userscripts
 // @supportURL  https://github.com/marascript/userscripts/issues
