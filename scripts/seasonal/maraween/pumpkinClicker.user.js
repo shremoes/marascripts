667c5fa64e886f390cc6d363c9bdbf3463d9127e
marascript <113377983+marascript@users.noreply.github.com>
2023-10-22 13:34:45 -0500

    Update pumpkinClicker.user.js

--- scripts/seasonal/maraween/pumpkinClicker.user.js
+++ scripts/seasonal/maraween/pumpkinClicker.user.js
@@ -3,7 +3,7 @@
 // @namespace   Marascripts
 // @description Clicks pumpkins as they appear around the site.
 // @author      marascripts
-// @version     1.1.1
+// @version     2.0.0
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @match       https://www.marapets.com/*
@@ -17,18 +17,10 @@
 /*jshint -W117 */ // Avoid JSHint errors in editor for GM* functions
 
 (function () {
-    'use strict'
+    "use strict"
 
-    /**
-     * Needs to be changed every year.
-     */
-    const IMG_SRC = "https://images.marapets.com/items/2022-pumpkin.png"
-
-    let clicked = GM_getValue("clickedPumpkins", 0)
-    const pumpkin = document.querySelector(`.pumpkineventbox img[src='${IMG_SRC}']`)
-    if (pumpkin) {
-        clicked += 1
-        GM_setValue("clickedPumpkins", clicked)
-        window.open(pumpkin.parentElement.href, 'pumpkin', 'width=100,height=100,top=0,left=0,resizable=no')
+    const balloon = document.querySelector(".pumpkineventbox a")
+    if (balloon) {
+        window.open(balloon.href, 'balloon', 'width=100,height=100,top=0,left=0,resizable=no')
     }
 })()
