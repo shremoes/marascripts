a0cb1542f70e75d505cc00030f67b8687cb0c0b1
Billy Schulz <will.js@protonmail.com>
2024-02-02 11:52:26 -0600

    Add description

--- scripts/scrollToTop.user.js
+++ scripts/scrollToTop.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        Scroll to Top
 // @namespace   Marascripts
-// @description Automatically selects the best value job.
+// @description Adds a button on every page which scrolls to the top.
 // @author      marascripts
-// @version     1.0.0
+// @version     1.0.1
 // @grant       none
 // @match       https://www.marapets.com*
 // @downloadURL https://raw.githubusercontent.com/marascript/userscripts/master/scripts/scrollToTop.user.js
@@ -15,15 +15,15 @@
 (() => {
     'use strict'
 
-    const toTopButton = document.createElement("a")
-    toTopButton.innerText = "⬆️"
-    toTopButton.style.cursor = "pointer"
-    toTopButton.style.fontSize = "3em"
+    const toTopButton = document.createElement('a')
+    toTopButton.innerText = '⬆️'
+    toTopButton.style.cursor = 'pointer'
+    toTopButton.style.fontSize = '3em'
 
     // Anchor to bottom of page
-    toTopButton.style.position = "fixed"
-    toTopButton.style.bottom = "5%"
-    toTopButton.style.right = "2%"
+    toTopButton.style.position = 'fixed'
+    toTopButton.style.bottom = '5%'
+    toTopButton.style.right = '2%'
 
     // Make button slightly opaque until hover
     toTopButton.style.opacity = 0.25
@@ -33,5 +33,5 @@
     // Scroll to top, without modifying URL
     toTopButton.onclick = () => window.scrollTo(0, 0)
 
-    document.querySelector("body").appendChild(toTopButton)
+    document.querySelector('body').appendChild(toTopButton)
 })()
