72773325de5f4fbc33decdf5e1688753d61b499d
marascript <113377983+marascript@users.noreply.github.com>
2024-05-03 11:18:10 -0500

    Clearer settings, split into functions

--- scripts/shopPricer.user.js
+++ scripts/shopPricer.user.js
@@ -3,7 +3,7 @@
 // @namespace   Marascripts
 // @description Automatically prices shop, requires the Gift Box.
 // @author      marascripts
-// @version     1.1.0
+// @version     1.2.0
 // @grant       none
 // @match       https://www.marapets.com/viewstock.php*
 // @downloadURL https://raw.githubusercontent.com/marascript/userscripts/master/scripts/shopPricer.user.js
@@ -15,59 +15,59 @@
 // TODO: Add a button instead of the shopkeeper
 
 (() => {
-    'use strict';
-  
-    const TIMEOUT = 1200000;
-    const IGNORE_LARGE_DROPS = 0;
-    const IGNORE_MEDIUM_DROPS = 0;
-  
-    const noReprice = document.querySelector('.maralayoutmiddle .bigger.banned.middleit');
-    const changed = document.querySelector('.maralayoutmiddle .bigger.middleit').innerText.search('You have changed');
-    const autoPriced = document.querySelector('.maralayoutmiddle .bigger.middleit').innerText.search('has changed');
-  
-    // Click the "Auto Price" button, if none of the messages are present
-    if (autoPriced === -1 && changed === -1 && !noReprice) {
-      document.querySelector("input[value='Auto Price']").click();
+  'use strict';
+
+
+  /**
+   * Settings
+   * 
+   * TIMEOUT - Set to amount of milliseconds to wait before restarting
+   * IGNORE_LARGE_DROPS - Set to 1 to ignore large price drops (red box)
+   * IGNORE_MEDIUM_DROPS - Set to 1 to ignore medium price drops (yellow box)
+   */
+  const TIMEOUT = 1200000;
+  const IGNORE_LARGE_DROPS = 0;
+  const IGNORE_MEDIUM_DROPS = 0;
+
+
+  function getNextStep() {
+    const message = document.querySelector('.maralayoutmiddle .bigger.middleit')
+    const changed = message.innerText.includes('have');
+    const autoPriced = message.innerText.includes('has');
+    const noReprice = message.innerText.includes('not');
+
+    if (autoPriced) { updatePrices() }
+    if (noReprice || changed) { nextPageOrRestart() }
+    else { document.querySelector("input[value='Auto Price']").click(); }
+  }
+
+  function nextPageOrRestart() {
+    const pageButtons = document.querySelectorAll('.pages_all .pages_each a');
+    const secondToLastPageButton = pageButtons[pageButtons.length - 2];
+
+    if (secondToLastPageButton.innerText === 'Next ›') {
+      secondToLastPageButton.click();
+    } else {
+      setTimeout(() => {
+        location.href = 'https://www.marapets.com/viewstock.php?page=1';
+      }, TIMEOUT);
     }
-  
-    // Click the "Update Prices" button, if the auto pricer message is there
-    if (autoPriced !== -1) {
-      if (!IGNORE_LARGE_DROPS) {
-        const largeDrop = document.querySelectorAll('.marapets_border15');
-        if (largeDrop.length !== 0) {
-          ignoreReprice(largeDrop);
-        }
-      }
-  
-      if (!IGNORE_MEDIUM_DROPS) {
-        const mediumDrop = document.querySelectorAll('.marapets_border14');
-        if (mediumDrop.length !== 0) {
-          ignoreReprice(mediumDrop);
-        }
-      }
-  
-      document.querySelector("input[value='Update Prices']").click();
-    }
-  
-    // Go to the next page, or alert that we are finished
-    if (noReprice || changed !== -1) {
-      const secondToLastPageButton = [...document.querySelectorAll('.pages_all')[0].querySelectorAll('.pages_each a')].slice(-2, -1)[0];
-  
-      // If the second to last button says "Next >", click it otherwise notify we are finished
-      if (secondToLastPageButton.innerText === 'Next ›') {
-        secondToLastPageButton.click();
-      } else {
-        setTimeout(() => {
-          window.location.href = 'https://www.marapets.com/viewstock.php?page=1';
-        }, TIMEOUT);
-      }
-    }
-  
-    function ignoreReprice(items) {
-      items.forEach((item) => {
-        const oldPrice = item.querySelector('.mp').innerText.split('MP')[0].replace(/,/g, '');
-        item.querySelector('input').value = oldPrice;
-      });
-    }
-  })();
-  
+  }
+
+  function updatePrices() {
+    if (!IGNORE_LARGE_DROPS) { ignoreReprice('.marapets_border15'); }
+    if (!IGNORE_MEDIUM_DROPS) { ignoreReprice('.marapets_border14'); }
+
+    document.querySelector("input[value='Update Prices']").click();
+  }
+
+  function ignoreReprice(selector) {
+    const items = document.querySelectorAll(selector);
+    items.forEach((item) => {
+      const oldPrice = item.querySelector('.mp').innerText.split('MP')[0].replace(/,/g, '');
+      item.querySelector('input').value = oldPrice;
+    });
+  }
+
+  getNextStep()
+})();
