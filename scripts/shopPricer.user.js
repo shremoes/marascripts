fb6d9c054aff0167bfa5f110ec08ab67b8a49d71
marascript <113377983+marascript@users.noreply.github.com>
2024-05-03 10:10:04 -0500

    Refactor

--- scripts/shopPricer.user.js
+++ scripts/shopPricer.user.js
@@ -3,7 +3,7 @@
 // @namespace   Marascripts
 // @description Automatically prices shop, requires the Gift Box.
 // @author      marascripts
-// @version     1.0.0
+// @version     1.1.0
 // @grant       none
 // @match       https://www.marapets.com/viewstock.php*
 // @downloadURL https://raw.githubusercontent.com/marascript/userscripts/master/scripts/shopPricer.user.js
@@ -15,48 +15,59 @@
 // TODO: Add a button instead of the shopkeeper
 
 (() => {
-    "use strict"
-
-    /**
-     * * Set this to the amount of seconds to delay, in milliseconds
-     * */
-    const TIMEOUT = 150000
-
-    const noReprice = document.querySelector(".maralayoutmiddle .bigger.banned.middleit")
-    const changed = document.querySelector(".maralayoutmiddle .bigger.middleit").innerText.search("You have changed")
-    const autoPriced = document.querySelector(".maralayoutmiddle .bigger.middleit").innerText.search("has changed")
-
+    'use strict';
+  
+    const TIMEOUT = 150000;
+    const IGNORE_LARGE_DROPS = 1;
+    const IGNORE_MEDIUM_DROPS = 1;
+  
+    const noReprice = document.querySelector('.maralayoutmiddle .bigger.banned.middleit');
+    const changed = document.querySelector('.maralayoutmiddle .bigger.middleit').innerText.search('You have changed');
+    const autoPriced = document.querySelector('.maralayoutmiddle .bigger.middleit').innerText.search('has changed');
+  
     // Click the "Auto Price" button, if none of the messages are present
     if (autoPriced === -1 && changed === -1 && !noReprice) {
-        document.querySelector("input[value='Auto Price']").click()
+      document.querySelector("input[value='Auto Price']").click();
     }
-
+  
     // Click the "Update Prices" button, if the auto pricer message is there
     if (autoPriced !== -1) {
-        // const mediumDrop = document.querySelectorAll(".marapets_border14")
-        const largeDrop = document.querySelectorAll(".marapets_border15")
+      if (!IGNORE_LARGE_DROPS) {
+        const largeDrop = document.querySelectorAll('.marapets_border15');
         if (largeDrop.length !== 0) {
-            largeDrop.forEach((item) => {
-                const oldPrice = item.querySelector(".mp").innerText.split("MP")[0].replace(/,/g, "")
-                item.querySelector("input").value = oldPrice
-            })
+          ignoreReprice(largeDrop);
         }
-
-        document.querySelector("input[value='Update Prices']").click()
+      }
+  
+      if (!IGNORE_MEDIUM_DROPS) {
+        const mediumDrop = document.querySelectorAll('.marapets_border14');
+        if (mediumDrop.length !== 0) {
+          ignoreReprice(mediumDrop);
+        }
+      }
+  
+      document.querySelector("input[value='Update Prices']").click();
     }
-
+  
     // Go to the next page, or alert that we are finished
     if (noReprice || changed !== -1) {
-        const pageButtons = document.querySelectorAll(".pages_all")[0].querySelectorAll(".pages_each a") // Array of the page selector
-        const secondToLastPageButton = pageButtons[pageButtons.length - 2] // The second to last button
-
-        // If the second to last button says "Next >", click it otherwise notify we are finished
-        if (secondToLastPageButton.innerText === "Next ›") {
-            secondToLastPageButton.click()
-        } else {
-            setTimeout(() => {
-                window.location.href = "https://www.marapets.com/viewstock.php?page=1"
-            }, TIMEOUT)
-        }
+      const secondToLastPageButton = [...document.querySelectorAll('.pages_all')[0].querySelectorAll('.pages_each a')].slice(-2, -1)[0];
+  
+      // If the second to last button says "Next >", click it otherwise notify we are finished
+      if (secondToLastPageButton.innerText === 'Next ›') {
+        secondToLastPageButton.click();
+      } else {
+        setTimeout(() => {
+          window.location.href = 'https://www.marapets.com/viewstock.php?page=1';
+        }, TIMEOUT);
+      }
     }
-})()
+  
+    function ignoreReprice(items) {
+      items.forEach((item) => {
+        const oldPrice = item.querySelector('.mp').innerText.split('MP')[0].replace(/,/g, '');
+        item.querySelector('input').value = oldPrice;
+      });
+    }
+  })();
+  
