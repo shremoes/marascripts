76adc19e71e92d32ef58895c869885db73207ca2
Billy Schulz <will.js@protonmail.com>
2024-05-08 22:31:12 -0500

    Settings, formatting, and refactor

--- scripts/spam.user.js
+++ scripts/spam.user.js
@@ -3,7 +3,7 @@
 // @namespace   Marascripts
 // @description Posts a random emoji every 2 minutes.
 // @author      marascripts
-// @version     1.0.1
+// @version     2.0.0
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @match       https://www.marapets.com/topics.php*
@@ -16,39 +16,33 @@
 (() => {
     'use strict'
 
+    /**
+     * * SETTINGS
+     * ? MY_SPAM - Set to spam threads ID (...topics.php?post=1&id=111111&...)
+     * ! MAX_TIME_TO_WAIT - MIN_TIME_TO_WAIT needs to be less than 60000
+     * ? MAX_TIME_TO_WAIT - Max time in milliseconds between posts (125000 default)
+     * ? MIN_TIME_TO_WAIT - Minimum time in milliseconds between posts (70000 default)
+     */
+    const MY_SPAM = 111111
+    const MAX_TIME_TO_WAIT = 125000
+    const MIN_TIME_TO_WAIT = 70000
+
     const URL = document.URL
 
-    if (URL.includes("fid=20")) {
-        /**
-         * * Set this to your spam threads ID
-         * ? The ID is the #: https://www.marapets.com/topics.php?post=1&id=#######&fid=20
-         */
-        const MY_SPAM = 111111
+    if (URL.includes(`id=${MY_SPAM}`) && URL.includes('fid=20')) {
+        setTimeout(() => {
+            const emoticons = [...document.querySelectorAll('.postmessage_emoticons a')].sort(() => Math.random() - 0.5)
+            const lastEmoticon = GM_getValue('lastEmoji', '')
 
-        // Avoid posting more than every sixty seconds, or too fast
-        const MAX_TIME_TO_WAIT = 125000
-        const MIN_TIME_TO_WAIT = 70000
-
-        if (URL.includes(`id=${MY_SPAM}`)) {
-            setTimeout(() => {
-                // Get all the emojis, and shuffle the array
-                const emoticons = [...document.querySelectorAll(".postmessage_emoticons a")].sort(() => Math.random() - 0.5)
-
-                // Get the last emoji used (the onclick)
-                const lastEmoticon = GM_getValue("lastEmoji", "")
-
-                for (const emoticon in emoticons) {
-                    const emojiType = emoticons[emoticon].getAttribute("onclick")
-                    // Compare the onclick event to the last, to avoid double post
-                    // Pick the first one that is different, and make a new post
-                    if (emojiType !== lastEmoticon) {
-                        emoticons[emoticon].click()
-                        GM_setValue("lastEmoji", emojiType) // Save the new emoji for next post
-                        document.querySelector(".postmessage_button.forums_sendbutton input").click()
-                        break
-                    }
+            for (const emoticon in emoticons) {
+                const emojiType = emoticons[emoticon].getAttribute('onclick')
+                if (emojiType !== lastEmoticon) {
+                    emoticons[emoticon].click()
+                    GM_setValue('lastEmoji', emojiType)
+                    document.querySelector('.postmessage_button.forums_sendbutton input').click()
+                    break
                 }
-            }, Math.random() * (MAX_TIME_TO_WAIT - MIN_TIME_TO_WAIT) + MIN_TIME_TO_WAIT)
-        }
+            }
+        }, Math.random() * (MAX_TIME_TO_WAIT - MIN_TIME_TO_WAIT) + MIN_TIME_TO_WAIT)
     }
 })()
