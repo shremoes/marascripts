b1f786d422627f897ae3187e620b4af3ad1ef12e
Billy Schulz <will.js@protonmail.com>
2024-05-08 22:59:12 -0500

    Flip if-else

--- scripts/drewShusanHelper.user.js
+++ scripts/drewShusanHelper.user.js
@@ -3,7 +3,7 @@
 // @namespace   Marascripts
 // @description Does Drew and Shusan, and prompts when item is missing.
 // @author      marascripts
-// @version     1.1.1
+// @version     1.2.0
 // @grant       none
 // @match       https://www.marapets.com/drew.php*
 // @match       https://www.marapets.com/socks.php*
@@ -24,32 +24,31 @@
 (() => {
     'use strict'
 
-    if (document.querySelector(".flex-table2")) {
-        const itemsRequested = document.querySelectorAll(".dopricecheck").length
+    if (document.querySelector('.flex-table2')) {
+        const itemsRequested = document.querySelectorAll('.dopricecheck').length
         const checks = document.querySelectorAll("img[src='https://images.marapets.com/tick.png']").length
 
-        if (checks !== itemsRequested) {
-            alert("Missing items.")
-        }
+        if (checks !== itemsRequested) { alert('Missing items.') }
 
         else {
             const captcha = document.querySelector("input[name='code']")
             const complete = document.querySelector("input[type='submit']")
-            if (captcha) {
+
+            if (!captcha) { complete.click() }
+
+            else {
                 captcha.focus()
                 captcha.oninput = () => {
                     if (captcha.value.length === 6) {
                         complete.click()
                     }
                 }
-            } else {
-                complete.click()
             }
         }
     }
 
-    const questAgain = document.querySelector(".middleit input")
-    if (questAgain.value.includes("Again")) {
+    const questAgain = document.querySelector('.middleit input')
+    if (questAgain.value.includes('Again')) {
         questAgain.click()
     }
 })()
